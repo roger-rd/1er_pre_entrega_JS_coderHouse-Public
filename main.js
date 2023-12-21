57fa2c4c7f33c45bdcda110ee69f196ad2f5368f
roger rodriguez <rogerdavid.rd@gmail.com>
2023-12-20 23:14:03 -0300

    modificadon codigo agregadno un objeto y un array

--- main.js
+++ main.js
@@ -1,138 +1,119 @@
 let nombre = "";
-let entrada = "";
-let principal = "";
-let bebida = "";
-let precioEntrada = 0;
-let precioPrincipal = 0;
-let precioBebida = 0;
+let mesa = "";
 let subTotal = 0;
-let total = 0;
 let propinaPorcentaje = 0;
 let detallesPedido = `Detalles del pedido:\n`;
 
-while (nombre === "") {
-    nombre = prompt("Bienvenido, indique su nombre para ser atendido").trim();
+const platos = {
+    entrada: [
+        { id: 1, nombre: "empanadas", precio: 3.5 },
+        { id: 2, nombre: "arepitas", precio: 4.0 },
+        { id: 3, nombre: "pan con queso", precio: 2.5 },
+        { id: 4, nombre: "facturas", precio: 5.0 },
+        { id: 0, nombre: "0", precio: 0 },
+    ],
+    principal: [
+        { id: 20, nombre: "pasticho", precio: 9.5 },
+        { id: 21, nombre: "ojo de bife", precio: 14.0 },
+        { id: 22, nombre: "costillas de cerdo", precio: 2.5 },
+        { id: 23, nombre: "sopa", precio: 5.0 },
+        { id: 0, nombre: "0", precio: 0 },
+    ],
+    bebida: [
+        { id: 30, nombre: "coca cola", precio: 1.5 },
+        { id: 31, nombre: "pepsi cola", precio: 1.5 },
+        { id: 32, nombre: "jugo natural", precio: 2.0 },
+        { id: 33, nombre: "cerveza", precio: 3.0 },
+        { id: 0, nombre: "0", precio: 0 },
+    ],
+};
+
+const obtenerOpcionPorID = (tipoPlato) => {
+    let opcion;
+        do {
+            opcion = prompt(`Ingrese el número de la opción de ${tipoPlato}: \n${obtenerOpcionesPorID(platos[tipoPlato])}`            );
+                if (!esOpcionValidaPorID(opcion, platos[tipoPlato])) {
+                    alert(`Por favor, ingrese un número de opción de ${tipoPlato} válido.`);
+                }
+        } while (!esOpcionValidaPorID(opcion, platos[tipoPlato]));
+    return platos[tipoPlato].find((plato) => plato.id === parseInt(opcion));
+};
+
+const obtenerOpcionesPorID = (listaPlatos) => {
+    return listaPlatos.map((plato) =>`\n ${plato.id}. ${plato.nombre} .. $${plato.precio.toFixed(2)}`).join(", ");
+};
+
+const esOpcionValidaPorID = (opcion, listaPlatos) => {
+    opcion = parseInt(opcion);
+    return listaPlatos.some((plato) => plato.id === opcion);
+};
+
+//inicia el codigo solicitando numero de mesa y nombre cliente
+do {
+    mesa = parseInt(prompt("Ingrese numero de mesa para tomar el pedido").trim());
+
+    if (isNaN(mesa) || mesa <= 0) {
+        alert("Por favor, ingrese un numero de mesa válido.");
+        continue;
+    }
+
+    nombre = prompt("Solicitar nombre del cliente").trim();
 
     if (nombre === "") {
-    alert("Por favor, ingrese un nombre válido.");
+        alert("Por favor, ingrese un nombre válido.");
     }
-}
-
-function esOpcionValida(opcion) {
-    // Limpiar espacios y convertir a minúsculas
-    opcion = opcion.trim().toLowerCase();
-
-    // Verificar si la opción es una de las opciones válidas
-    return  opcion === "empanadas" || opcion === "arepitas" || opcion === "pan con queso" || opcion === "facturas" ||
-            opcion === "pasticho" || opcion === "ojo de bife" || opcion === "costillas de cerdo" ||
-            opcion === "sopa" || opcion === "coca cola" || opcion === "pepsi cola" || opcion === "jugo natural" || opcion === "cerveza";
-}
+} while (nombre === "" || isNaN(mesa) || mesa <= 0);
 
 // Bucle para permitir al usuario agregar varios platos
 do {
-    while (entrada === "" || !esOpcionValida(entrada)) {
-    entrada = prompt(
-        "Ingrese su opción de entrada (Opciones: empanadas .. $3.50, arepitas .. $4, Pan con Queso .. $2.50, Facturas .. $5)"
-    );
+  const entrada = obtenerOpcionPorID("entrada");
+  const principal = obtenerOpcionPorID("principal");
+  const bebida = obtenerOpcionPorID("bebida");
 
-    if (entrada === "" || !esOpcionValida(entrada)) {
-        alert("Por favor, ingrese una opción de entrada válida.");
-    } else {
-        switch (entrada.toLowerCase()) {
-        case "empanadas":
-            precioEntrada = 3.5;
-            break;
-        case "arepitas":
-            precioEntrada = 4.0;
-            break;
-        case "pan con queso":
-            precioEntrada = 2.5;
-            break;
-        case "facturas":
-            precioEntrada = 5.0;
-            break;
-            };
-        };
-    };
-
-detallesPedido += `Entrada: ${entrada} - $${precioEntrada.toFixed(2)}\n`;
-
-    while (principal === "" || !esOpcionValida(principal)) {
-        principal = prompt("Ingrese su opción de principal (Opciones: Pasticho .. $9.50, ojo de bife .. $14, Costillas de Cerdo .. $2.50, Sopa .. $8)");
-
-        if (principal === "" || !esOpcionValida(principal)) {
-            alert("Por favor, ingrese una opción de principal válida.");
-        } else {
-            switch (principal.toLowerCase()) {
-                case "pasticho":
-                precioPrincipal = 9.5;
-                break;
-                case "ojo de bife":
-                precioPrincipal = 14.0;
-                break;
-                case "costillas de cerdo":
-                precioPrincipal = 2.5;
-                break;
-                case "sopa":
-                precioPrincipal = 5.0;
-                break;
-                };
-        };
-    };
-
-detallesPedido += `Principal: ${principal} - $${precioPrincipal.toFixed(2)}\n`;
-
-    while (bebida === "" || !esOpcionValida(bebida)) {
-        bebida = prompt("Ingrese su opción de bebida (Opciones: Coca Cola .. $1.50, Pepsi Cola .. $1.50, Jugo Natural .. $2, Cerveza .. $3)");
-
-        if (bebida === "" || !esOpcionValida(bebida)) {
-            alert("Por favor, ingrese una opción de bebida válida.");
-        } else {
-            switch (bebida.toLowerCase()) {
-                case "coca cola":
-                precioBebida = 1.5;
-                break;
-                case "pepsi cola":
-                precioBebida = 1.5;
-                break;
-                case "jugo natural":
-                precioBebida = 2.0;
-                break;
-                case "cerveza":
-                precioBebida = 3.0;
-                break;
-            }
-        }
-    }
-
-detallesPedido += `Bebida: ${bebida} - $${precioBebida.toFixed(2)}\n`;
-
-  //cuenta
-    subTotal += precioEntrada + precioPrincipal + precioBebida;
   // Preguntar al usuario si desea agregar otro plato
-    let respuesta = prompt("¿Desea agregar otro plato? (Sí/No)");
-        if (respuesta && respuesta.toLowerCase() === "si") {
-            entrada = "";
-            principal = "";
-            bebida = "";
-        } else {
-            break;
-        }
+//   const respuesta = prompt("¿Desea agregar otro plato? (Sí/No)");
+//   if (!respuesta || respuesta.toLowerCase() !== "si") {
+//     break;
+//   }
+let respuesta;
+
+do {
+  respuesta = prompt("¿Desea agregar otro plato? (Sí/No)").trim().toLowerCase();
+
+  if (respuesta !== "si" && respuesta !== "no") {
+    alert("Por favor, ingrese 'si' para Sí o 'no' para No.");
+  }
+} while (respuesta !== "si" && respuesta !== "no");
+
+  // Agregar detalles del pedido
+  detallesPedido += `${entrada.nombre}: $${entrada.precio.toFixed(2)}\n`;
+  detallesPedido += `${principal.nombre}: $${principal.precio.toFixed(2)}\n`;
+  detallesPedido += `${bebida.nombre}: $${bebida.precio.toFixed(2)}\n`;
+
+  // Calcular subtotal
+  subTotal += entrada.precio + principal.precio + bebida.precio;
+
+  
+  
 } while (true);
 
 // Solicitar al usuario el porcentaje de propina
 propinaPorcentaje = parseFloat(
-    prompt("Ingrese el porcentaje de propina (por ejemplo, 10 para el 10%)")
+  prompt("Ingrese el porcentaje de propina (por ejemplo, 10 para el 10%)")
 );
 
-//Propina
-total = subTotal + (subTotal * propinaPorcentaje) / 100;
+// Calcular total
+const total = subTotal + (subTotal * propinaPorcentaje) / 100;
 
-// detalles del pedido al mensaje final
+// Agregar detalles del pedido al mensaje final
 detallesPedido += `\nSubtotal: $${subTotal.toFixed(2)}`;
-detallesPedido += `\nPropina (${propinaPorcentaje}%): $${((subTotal * propinaPorcentaje) /100).toFixed(2)}`;
+detallesPedido += `\nPropina (${propinaPorcentaje}%): $${(
+  (subTotal * propinaPorcentaje) /
+  100
+).toFixed(2)}`;
 detallesPedido += `\nTotal: $${total.toFixed(2)}`;
 
 // Mostrar el mensaje final con los detalles del pedido
 alert(
-    `Gracias ${nombre} Por ser cliente de nuestro Restaurante,  \n${detallesPedido}`);
+  `CUENTA MESA ${mesa}\n ${nombre} Gracias  por ser cliente de nuestro Restaurante,\n${detallesPedido}`
+);
